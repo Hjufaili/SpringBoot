4d2492f28a4edce0b230ce58ed47293272f265cf
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-17 14:36:08 +0400

    Task: CRUD Apis is Done

--- src/main/java/com/codeline/demo/Course.java
+++ src/main/java/com/codeline/demo/Course.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class Course {
 
     private Map<Integer, String> courses = new HashMap<>();
