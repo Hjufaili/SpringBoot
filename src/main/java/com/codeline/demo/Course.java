28490b6879784a422b2fc5c12f45aa1695395064
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-17 12:39:38 +0400

    POST (Create course with name)

--- src/main/java/com/codeline/demo/Course.java
+++ src/main/java/com/codeline/demo/Course.java
@@ -1,6 +1,21 @@
 package com.codeline.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Course {
 
+    private Map<Integer, String> courses = new HashMap<>();
+    private Integer couresID = 1;
 
+
+    @PostMapping("/create")
+    public String createCourse(@RequestParam String name) {
+        courses.put(couresID, name);
+        return "Course create (name: " + name + " ID: " + couresID++ + ")";
+    }
 }
