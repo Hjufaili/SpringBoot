7b7ab556951a7eebade5a87d7e69a9e2f0db26b7
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-19 09:35:01 +0400

    update CourseController for service

--- src/main/java/com/codeline/demo/controllers/CourseController.java
+++ src/main/java/com/codeline/demo/controllers/CourseController.java
@@ -1,6 +1,8 @@
 package com.codeline.demo.controllers;
 
 import com.codeline.demo.entity.Course;
+import com.codeline.demo.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -9,20 +11,15 @@ import java.util.*;
 @RestController
 public class CourseController {
 
-    private Map<Integer, String> courses = new HashMap<>();
-    private List<Course> coursesList = new ArrayList<>();
-    private Integer courseID = 1;
+    @Autowired
+    private CourseService courseService;
 
 
     @PostMapping("/create")
     public String createCourse(@RequestBody Course requestObj) {
-        requestObj.setCourseId(courseID);
-        requestObj.setCreatedDate(new Date());
-        requestObj.setIsActive(true);
+        courseService.createCourse(requestObj);
 
-        coursesList.add(requestObj);
-
-        return "Course create (name: " + requestObj.getCourseName() + " ID: " + courseID++ + ")";
+        return "Course create (name: " + requestObj.getCourseName() + " ID: " + requestObj.getCourseId() + ")";
     }
 
 /*    public String createCourse(@RequestParam String name) {
@@ -30,16 +27,15 @@ public class CourseController {
         return "Course create (name: " + name + " ID: " + courseID++ + ")";
     }*/
 
-    @PostMapping("/save")
+    /*@PostMapping("/save")
     public String saveCourse(@RequestBody String name) {
         courses.put(courseID, name);
         return "Course create (name: " + name + " ID: " + courseID++ + ")";
-    }
+    }*/
 
     @GetMapping("/getAll")
     public List<Course> getAll() {
-        purgeOldInactiveCourses();
-        return coursesList;
+        return courseService.getAll();
     }
 
 //    @GetMapping("/getAll")
@@ -58,12 +54,7 @@ public class CourseController {
 
     @GetMapping("/getById")
     public Course getCourse(@RequestParam int id) {
-        for (Course s : coursesList) {
-            if (s.getCourseId() == id && s.getIsActive()) {
-                return s;
-            }
-        }
-        return Course.builder().build();
+        return courseService.getCourse(id);
     }
 //    public String getCourseByID(@RequestParam int id) {
 //        return courses.getOrDefault(id, "course not found");
@@ -71,36 +62,17 @@ public class CourseController {
 
     @GetMapping("/getByID/{id}")
     public String getCourseById(@PathVariable int id) {
-        return courses.getOrDefault(id, "course not found");
+        return courseService.getCourseById(id);
     }
 
     @GetMapping("/getByName")
     public String getCourseByName(@RequestParam String name) {
-        for (Map.Entry<Integer, String> entry : courses.entrySet()) {
-            if (entry.getValue().equalsIgnoreCase(name)) {
-                return "Course found: ID = " + entry.getKey() + ", Name = " + entry.getValue();
-            }
-        }
-        return "Course not found";
+        return courseService.getCourseByName(name);
     }
 
     @PutMapping("/update")
     public String updateCourse(@RequestBody Course updateObjFromUser) {
-        if (updateObjFromUser != null && updateObjFromUser.getCourseId() != null) {
-
-            Course existingStudentToUpdate = findCourseById(updateObjFromUser.getCourseId());
-
-            coursesList.remove(existingStudentToUpdate);
-
-            existingStudentToUpdate.setCourseName(updateObjFromUser.getCourseName());
-            existingStudentToUpdate.setInstructor(updateObjFromUser.getInstructor());
-            existingStudentToUpdate.setUpdateDate(new Date());
-
-            coursesList.add(existingStudentToUpdate);
-
-            return "Course updated successfully";
-        }
-        return "Course not found";
+        return courseService.updateCourse(updateObjFromUser);
     }
 //    public String updateCourse(@RequestParam int id, @RequestParam String name) {
 //        if (courses.containsKey(id)) {
@@ -112,47 +84,14 @@ public class CourseController {
 
     @DeleteMapping("/delete/{id}")
     public String deleteCourse(@PathVariable int id) {
-        Course existingCourseToUpdate = findCourseById(id);
-
-        if (existingCourseToUpdate.getCourseId() > 0) {
-            // Remove from current list
-            coursesList.remove(existingCourseToUpdate);
-
-            existingCourseToUpdate.setIsActive(false);
-            existingCourseToUpdate.setUpdateDate(new Date());
-            existingCourseToUpdate.setDeletedAt(new Date()); // mark deletion time
-
-            coursesList.add(existingCourseToUpdate);
-
-            return "Course deleted successfully (moved to inactive list)";
-        } else {
-            return "Invalid id";
-        }
+        return courseService.deleteCourse(id);
     }
 
     @PutMapping("/restore/{id}")
     public String restoreCourse(@PathVariable int id) {
-        Course existing = findCourseById(id);
+        return courseService.restoreCourse(id);
 
-        // Not found (your findStudentById returns courseId = -1)
-        if (existing == null || existing.getCourseId() <= 0) {
-            return "Course not found";
-        }
-
-        // Remove old copy
-        coursesList.remove(existing);
-
-        // Restore it
-        existing.setIsActive(true);
-        existing.setUpdateDate(new Date());
-        existing.setDeletedAt(null); // very important
-
-        // Add back as active
-        coursesList.add(existing);
-
-        return "Course restored successfully";
     }
-
 
 
     //    public String deleteCourse(@PathVariable int id) {
@@ -163,25 +102,6 @@ public class CourseController {
 //    }
 //
 //
-    public Course findCourseById(int id) {
-        for (Course s : coursesList) {
-            if (s.getCourseId() == id) {
-                return s;
-            }
-        }
-        return Course.builder().courseId(-1).build();
-    }
-
-    public void purgeOldInactiveCourses() {
-        Date now = new Date();
-        long oneDayMillis = 24L * 60 * 60 * 1000;
-
-        coursesList.removeIf(course ->
-                Boolean.FALSE.equals(course.getIsActive()) &&
-                        course.getDeletedAt() != null &&
-                        (now.getTime() - course.getDeletedAt().getTime()) > oneDayMillis
-        );
-    }
 
 
 }
