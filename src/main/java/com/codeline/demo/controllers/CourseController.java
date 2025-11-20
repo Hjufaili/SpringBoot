02eec8287c132b1636a22fd7008de80f523cbc46
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-20 08:37:05 +0400

    add Http response code

--- src/main/java/com/codeline/demo/controllers/CourseController.java
+++ src/main/java/com/codeline/demo/controllers/CourseController.java
@@ -3,6 +3,8 @@ package com.codeline.demo.controllers;
 import com.codeline.demo.entity.Course;
 import com.codeline.demo.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -16,10 +18,9 @@ public class CourseController {
 
 
     @PostMapping("/create")
-    public String createCourse(@RequestBody Course requestObj) {
+    public ResponseEntity createCourse(@RequestBody Course requestObj) {
         courseService.createCourse(requestObj);
-
-        return "Course create (name: " + requestObj.getCourseName() + " ID: " + requestObj.getCourseId() + ")";
+        return ResponseEntity.status(HttpStatus.CREATED).body("Course create (name: " + requestObj.getCourseName() + " ID: " + requestObj.getCourseId() + ")");
     }
 
 /*    public String createCourse(@RequestParam String name) {
@@ -34,8 +35,8 @@ public class CourseController {
     }*/
 
     @GetMapping("/getAll")
-    public List<Course> getAll() {
-        return courseService.getAll();
+    public ResponseEntity getAll() {
+        return ResponseEntity.ok(courseService.getAll());
     }
 
 //    @GetMapping("/getAll")
@@ -53,21 +54,21 @@ public class CourseController {
 //    }
 
     @GetMapping("/getById")
-    public Course getCourse(@RequestParam int id) {
+    public ResponseEntity getCourse(@RequestParam int id) {
         return courseService.getCourse(id);
     }
 //    public String getCourseByID(@RequestParam int id) {
 //        return courses.getOrDefault(id, "course not found");
 //    }
 
-    @GetMapping("/getByID/{id}")
+    /*@GetMapping("/getByID/{id}")
     public String getCourseById(@PathVariable int id) {
         return courseService.getCourseById(id);
-    }
+    }*/
 
     @GetMapping("/getByName")
-    public String getCourseByName(@RequestParam String name) {
-        return courseService.getCourseByName(name);
+    public ResponseEntity getCourseByName(@RequestParam String name) {
+        return courseService.getCourseByCourseName(name);
     }
 
     @PutMapping("/update")
