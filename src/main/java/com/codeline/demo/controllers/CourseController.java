e93aed4561f57ad7de87872c31a98fdf664aff39
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-23 13:41:25 +0400

    integration with CourseService

--- src/main/java/com/codeline/demo/controllers/CourseController.java
+++ src/main/java/com/codeline/demo/controllers/CourseController.java
@@ -3,8 +3,6 @@ package com.codeline.demo.controllers;
 import com.codeline.demo.entity.Course;
 import com.codeline.demo.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -18,91 +16,35 @@ public class CourseController {
 
 
     @PostMapping("/create")
-    public ResponseEntity createCourse(@RequestBody Course requestObj) {
-        courseService.createCourse(requestObj);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Course create (name: " + requestObj.getCourseName() + " ID: " + requestObj.getCourseId() + ")");
+    public Course createCourse(@RequestBody Course requestObj) {
+        Course course = courseService.createCourse(requestObj);
+        return course;
     }
 
-/*    public String createCourse(@RequestParam String name) {
-        courses.put(courseID, name);
-        return "Course create (name: "+ name + " ID: " + courseID++ + ")";
-    }*/
-
-    /*@PostMapping("/save")
-    public String saveCourse(@RequestBody String name) {
-        courses.put(courseID, name);
-        return "Course create (name: " + name + " ID: " + courseID++ + ")";
-    }*/
 
     @GetMapping("/getAll")
-    public ResponseEntity getAll() {
-        return ResponseEntity.ok(courseService.getAll());
+    public List<Course> getAll() {
+        List<Course> courses = courseService.getAllCourses();
+        return courses;
     }
 
-//    @GetMapping("/getAll")
-//    public List<Course> getAllCourses() {
-//        List<Course> responseList = new ArrayList<>();
-//        for (Course s : coursesList) {
-//            if (s.getIsActive()) {
-//                responseList.add(s);
-//            }
-//        }
-//        return responseList;
-//    }
-//    public Map<Integer, String> getAllCourses() {
-//        return courses;
-//    }
 
     @GetMapping("/getById")
-    public ResponseEntity getCourse(@RequestParam int id) {
-        return courseService.getCourse(id);
-    }
-//    public String getCourseByID(@RequestParam int id) {
-//        return courses.getOrDefault(id, "course not found");
-//    }
-
-    /*@GetMapping("/getByID/{id}")
-    public String getCourseById(@PathVariable int id) {
+    public Course getCourse(@RequestParam int id) throws Exception {
         return courseService.getCourseById(id);
-    }*/
-
-    @GetMapping("/getByName")
-    public ResponseEntity getCourseByName(@RequestParam String name) {
-        return courseService.getCourseByCourseName(name);
     }
 
     @PutMapping("/update")
-    public ResponseEntity updateCourse(@RequestBody Course updateObjFromUser) {
+    public Course updateCourse(@RequestBody Course updateObjFromUser) throws Exception {
         return courseService.updateCourse(updateObjFromUser);
     }
-//    public String updateCourse(@RequestParam int id, @RequestParam String name) {
-//        if (courses.containsKey(id)) {
-//            courses.put(id, name);
-//            return "Course updated successfully";
-//        }
-//        return "Course not found";
-//    }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity deleteCourse(@PathVariable int id) {
-        return courseService.deleteCourse(id);
-    }
-
-    @PutMapping("/restore/{id}")
-    public ResponseEntity restoreCourse(@PathVariable int id) {
-        return courseService.restoreCourse(id);
+    public String deleteCourse(@PathVariable int id) throws Exception {
+        courseService.deleteCourse(id);
+        return "SUCCESS";
 
     }
-
-
-    //    public String deleteCourse(@PathVariable int id) {
-//        if (courses.remove(id) != null) {
-//            return "Course deleted successfully";
-//        }
-//        return "Course not found";
-//    }
-//
-//
 
 
 }
