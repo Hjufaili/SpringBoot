ec40597781acce2e3aeafe422e02c2f93f42054d
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-23 13:39:04 +0400

    add CourseRepository interface

---
+++ src/main/java/com/codeline/demo/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.codeline.demo.repositories;
+
+import com.codeline.demo.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Integer> {
+}
