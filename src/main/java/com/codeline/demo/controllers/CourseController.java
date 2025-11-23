3ebf1154bfa892dd8640031cf8f2be38c9d5bda8
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-23 14:00:13 +0400

    add HTTP Status Codes using ResponseEntity

--- src/main/java/com/codeline/demo/controllers/CourseController.java
+++ src/main/java/com/codeline/demo/controllers/CourseController.java
@@ -3,6 +3,8 @@ package com.codeline.demo.controllers;
 import com.codeline.demo.entity.Course;
 import com.codeline.demo.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -16,35 +18,47 @@ public class CourseController {
 
 
     @PostMapping("/create")
-    public Course createCourse(@RequestBody Course requestObj) {
+    public ResponseEntity<Course> createCourse(@RequestBody Course requestObj) {
         Course course = courseService.createCourse(requestObj);
-        return course;
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
     }
 
 
     @GetMapping("/getAll")
-    public List<Course> getAll() {
+    public ResponseEntity<List<Course>> getAll() {
         List<Course> courses = courseService.getAllCourses();
-        return courses;
+        return new ResponseEntity<>(courses, HttpStatus.OK);
     }
 
 
     @GetMapping("/getById")
-    public Course getCourse(@RequestParam int id) throws Exception {
-        return courseService.getCourseById(id);
+    public ResponseEntity<?> getCourse(@RequestParam int id) {
+        try {
+            Course course = courseService.getCourseById(id);
+            return new ResponseEntity<>(course, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/update")
-    public Course updateCourse(@RequestBody Course updateObjFromUser) throws Exception {
-        return courseService.updateCourse(updateObjFromUser);
+    public ResponseEntity<?> updateCourse(@RequestBody Course updateObjFromUser) {
+        try {
+            Course updatedCourse = courseService.updateCourse(updateObjFromUser);
+            return new ResponseEntity<>(updatedCourse, HttpStatus.OK); 
+        } catch (Exception e) {
+            return new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteCourse(@PathVariable int id) throws Exception {
-        courseService.deleteCourse(id);
-        return "SUCCESS";
-
+    public ResponseEntity<?> deleteCourse(@PathVariable int id) {
+        try {
+            courseService.deleteCourse(id);
+            return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND);
+        }
     }
-
-
 }
+
