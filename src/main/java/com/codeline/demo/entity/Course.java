77941cc3fda786b8e2876d51114b93c2d7154706
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-11-23 13:40:07 +0400

    configuration with database

--- src/main/java/com/codeline/demo/entity/Course.java
+++ src/main/java/com/codeline/demo/entity/Course.java
@@ -1,20 +1,30 @@
 package com.codeline.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Builder
 @Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer courseId;
     private String courseName;
     private String instructor;
     private Date createdDate;
     private Date updateDate;
     private Boolean isActive;
-    private Date deletedAt;
-
 }
