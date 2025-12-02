65290c442cffbe57a1cefe0756aac1b847ded10c
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-02 11:50:56 +0400

    Add Instructor Repository

---
+++ src/main/java/com/codeline/demo/repositories/InstructorRepository.java
@@ -0,0 +1,14 @@
+package com.codeline.demo.repositories;
+
+import com.codeline.demo.entity.Course;
+import com.codeline.demo.entity.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InstructorRepository extends JpaRepository<Instructor,Integer> {
+    List<Instructor> findByIsActiveTrue();
+
+}
