9f911b7a9fedf7341dda72150708c50209c47dbc
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-02 11:49:33 +0400

    Add Instructor entity

---
+++ src/main/java/com/codeline/demo/entity/Instructor.java
@@ -0,0 +1,46 @@
+package com.codeline.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    private Boolean isActive;
+
+    @OneToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+}
