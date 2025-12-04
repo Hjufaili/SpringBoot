593e2df631d82b233ee755bdba86e4a542454312
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-04 10:59:50 +0400

    Updated Instructor Repository

--- src/main/java/com/codeline/demo/repositories/InstructorRepository.java
+++ src/main/java/com/codeline/demo/repositories/InstructorRepository.java
@@ -1,6 +1,5 @@
 package com.codeline.demo.repositories;
 
-import com.codeline.demo.entity.Course;
 import com.codeline.demo.entity.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
