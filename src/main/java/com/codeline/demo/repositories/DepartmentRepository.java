ba6b59f340e148ca86a8c7ae342843e1d69daa5a
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-04 10:59:22 +0400

    Updated Department Repository

--- src/main/java/com/codeline/demo/repositories/DepartmentRepository.java
+++ src/main/java/com/codeline/demo/repositories/DepartmentRepository.java
@@ -1,14 +1,15 @@
 package com.codeline.demo.repositories;
 
-import com.codeline.demo.entity.Course;
 import com.codeline.demo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface DepartmentRepository extends JpaRepository<Department,Integer> {
+public interface DepartmentRepository extends JpaRepository<Department, Integer> {
     List<Department> findByIsActiveTrue();
 
+    Department findByName(String name);
 }
