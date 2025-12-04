dae309c84164d43fc1459ef677a0da7ffad3fbb6
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-04 10:54:49 +0400

    Add relationships with Instructor and Marks

--- src/main/java/com/codeline/demo/entity/Course.java
+++ src/main/java/com/codeline/demo/entity/Course.java
@@ -1,30 +1,37 @@
 package com.codeline.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Data
 @Entity
+@Table(name = "course")
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    @NotBlank(message = "Course name is required")
     private String courseName;
+
     @CreatedDate
     private LocalDateTime createdDate;
 
@@ -33,8 +40,10 @@ public class Course {
 
     private Boolean isActive;
 
-    @OneToOne(mappedBy = "course", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "course", fetch = FetchType.LAZY)
     private Instructor instructor;
 
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Marks> marks = new ArrayList<>();
 
 }
