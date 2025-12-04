47d6f3e7a128d89a421dc0b0e84b99227324d5bd
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-04 10:51:40 +0400

    create marks controller

---
+++ src/main/java/com/codeline/demo/controllers/MarksController.java
@@ -0,0 +1,70 @@
+package com.codeline.demo.controllers;
+
+
+import com.codeline.demo.entity.Department;
+import com.codeline.demo.entity.Marks;
+import com.codeline.demo.service.DepartmentService;
+import com.codeline.demo.service.MarksService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/marks")
+@CrossOrigin(origins = "*")
+public class MarksController {
+
+    @Autowired
+    private MarksService marksService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<Marks> createMarks(@Valid @RequestBody Marks requestObj) throws Exception {
+        Marks marks = marksService.createMarks(requestObj);
+        return new ResponseEntity<>(marks, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Marks>> getAllMarks() {
+        List<Marks> marks = marksService.getAllMarks();
+        return new ResponseEntity<>(marks, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<?> getMarksById(@PathVariable Integer id) {
+        try {
+            Marks marks = marksService.getMarksById(id);
+            return new ResponseEntity<>(marks, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Marks not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateMarks(@PathVariable Integer id,
+                                         @Valid @RequestBody Marks updateObjFromUser) {
+        try {
+            updateObjFromUser.setId(id);
+            Marks updatedMarks = marksService.updateMarks(updateObjFromUser);
+            return new ResponseEntity<>(updatedMarks, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Marks not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteMarks(@PathVariable int id) {
+        try {
+            marksService.deleteMarks(id);
+            return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Marks not found", HttpStatus.NOT_FOUND);
+        }
+    }
+}
