baa8997b6aa90de164ca8a7bd9fa2cd90cf745f6
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-09 09:19:01 +0400

    Add findMarksById method with Query

--- src/main/java/com/codeline/demo/repositories/MarksRepository.java
+++ src/main/java/com/codeline/demo/repositories/MarksRepository.java
@@ -3,13 +3,15 @@ package com.codeline.demo.repositories;
 import com.codeline.demo.entity.Instructor;
 import com.codeline.demo.entity.Marks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MarksRepository extends JpaRepository<Marks,Integer> {
+public interface MarksRepository extends JpaRepository<Marks, Integer> {
     List<Marks> findByIsActiveTrue();
-    List<Marks> findByCourseIdAndIsActiveTrue(Integer courseId);
-    List<Marks> findByStudentNameAndIsActiveTrue(String studentName);
+
+    @Query("SELECT m FROM Marks m WHERE m.id IN :ids AND m.isActive=true")
+    List<Marks> findMarkById(List<Integer> ids);
 }
