cfc8389e3c4842380d548d966fa86da5f7d83ed4
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-09 09:15:40 +0400

    Add DepartmentRepository method with Query

--- src/main/java/com/codeline/demo/repositories/DepartmentRepository.java
+++ src/main/java/com/codeline/demo/repositories/DepartmentRepository.java
@@ -2,6 +2,7 @@ package com.codeline.demo.repositories;
 
 import com.codeline.demo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,7 @@ public interface DepartmentRepository extends JpaRepository<Department, Integer>
     List<Department> findByIsActiveTrue();
 
     Department findByName(String name);
+
+    @Query("SELECT c FROM Department c WHERE c.id = :id AND c.isActive=true")
+    Department findDepartmentById(Integer id);
 }
