c808f0918952691579dd334f171aa452eb540c3e
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-09 09:13:13 +0400

    Implemented Constants class

---
+++ src/main/java/com/codeline/demo/helper/Constants.java
@@ -0,0 +1,22 @@
+package com.codeline.demo.helper;
+
+public class Constants {
+    public final static String SUCCESS = "Success";
+    public final static String BAD_REQUEST = "BAD REQUEST";
+    public final static String COURSE_CREATE_REQUEST_NAME_NOT_VALID="NAME NOT VALID";
+    public final static String COURSE_CREATE_REQUEST_INSTRUCTOR_ID_NOT_VALID="INSTRUCTOR NOT VALID";
+    public final static String COURSE_CREATE_REQUEST_MARK_ID_NOT_VALID="MARKS NOT VALID";
+    public final static String INSTRUCTOR_CREATE_REQUEST_NAME_NOT_VALID="NAME NOT VALID";
+    public final static String INSTRUCTOR_CREATE_REQUEST_DEPARTMENT_ID_NOT_VALID="DEPARTMENT NOT VALID";
+    public final static String DEPARTMENT_CREATE_REQUEST_NAME_NOT_VALID="NAME NOT VALID";
+    public final static String MARKS_CREATE_REQUEST_STUDENT_NAME_NOT_VALID="STUDENT NAME NOT VALID";
+    public final static String MARKS_CREATE_REQUEST_SCORE_NOT_VALID="SCORE NOT VALID";
+    public final static Integer SCORE_UPPER_LIMIT=100;
+    public final static Integer SCORE_LOWER_LIMIT=0;
+
+
+
+
+
+
+}
