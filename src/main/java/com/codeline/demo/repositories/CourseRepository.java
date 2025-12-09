08d47aa523c0ee0dd0b87e37af1dd6ab33334375
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-09 09:15:15 +0400

    Add findCourseById method with Query

--- src/main/java/com/codeline/demo/repositories/CourseRepository.java
+++ src/main/java/com/codeline/demo/repositories/CourseRepository.java
@@ -2,6 +2,7 @@ package com.codeline.demo.repositories;
 
 import com.codeline.demo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,8 @@ import java.util.List;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
     List<Course> findByIsActiveTrue();
-    Course findByCourseName(String courseName);}
+
+    @Query("SELECT c FROM Course c WHERE c.id = :id AND c.isActive=true")
+    Course findCourseById(Integer id);
+
+}
