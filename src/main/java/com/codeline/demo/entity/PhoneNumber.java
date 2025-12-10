b257109fa74d58d54c8f379d1f0a54a8236b478a
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-10 10:09:19 +0400

    Created PhoneNumber entity

---
+++ src/main/java/com/codeline/demo/entity/PhoneNumber.java
@@ -0,0 +1,44 @@
+package com.codeline.demo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class PhoneNumber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String number;
+    private String countryCode;
+    private Boolean isLandLine;
+
+    private Boolean isActive;
+
+    @CreatedDate
+    private Date createdDate;
+
+    @LastModifiedDate
+    private Date updatedDate;
+
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "student_id")
+    private Student student;
+}
