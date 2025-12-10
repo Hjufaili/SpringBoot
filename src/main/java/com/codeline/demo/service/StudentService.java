7a7f8f1130210382e8ebf95d38d24e73c126f92c
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-10 10:13:31 +0400

    Created StudentService

---
+++ src/main/java/com/codeline/demo/service/StudentService.java
@@ -0,0 +1,97 @@
+package com.codeline.demo.service;
+
+import com.codeline.demo.dto.CourseCreateResponse;
+import com.codeline.demo.dto.PhoneNumberCreateResponse;
+import com.codeline.demo.dto.StudentCreateRequest;
+import com.codeline.demo.dto.StudentCreateResponse;
+import com.codeline.demo.entity.Address;
+import com.codeline.demo.entity.Course;
+import com.codeline.demo.entity.PhoneNumber;
+import com.codeline.demo.entity.Student;
+import com.codeline.demo.helper.Constants;
+import com.codeline.demo.helper.HelperUtils;
+import com.codeline.demo.repositories.AddressRepository;
+import com.codeline.demo.repositories.PhoneNumberRepository;
+import com.codeline.demo.repositories.StudentRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    PhoneNumberRepository phoneNumberRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+
+    public StudentCreateResponse addStudent(StudentCreateRequest request) throws Exception {
+        Student student= StudentCreateRequest.convertToStudent(request);
+        student.setIsActive(Boolean.TRUE);
+
+        List<PhoneNumber> phoneNumbers = phoneNumberRepository.findPhoneNumberByNumber(request.getPhoneNumbers());
+        if (HelperUtils.isNotNull(phoneNumbers) && HelperUtils.isListNotEmpty(phoneNumbers)){
+            student.setPhoneNumbers(phoneNumbers);
+        }else {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_PHONE_NUMBER_NOT_VALID);
+        }
+
+        Address address = addressRepository.findAddressById(request.getAddressId());
+        if (HelperUtils.isNotNull(address)){
+            student.setAddress(address);
+        }else {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_ADDRESS_ID_NOT_VALID);
+        }
+
+        return StudentCreateResponse.convertToStudent(studentRepository.save(student));
+    }
+
+    public List<StudentCreateResponse> getAllStudents(){
+        List<Student> students = studentRepository.findByIsActiveTrue();
+        List<StudentCreateResponse> result = new ArrayList<>();
+        for (Student c : students) {
+            result.add(StudentCreateResponse.convertToStudent(c));
+        }
+        return result;
+    }
+
+    public StudentCreateResponse getStudentById(Integer id) throws Exception{
+        Student existingStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new Exception("Student not found"));
+        if (existingStudent.getIsActive()) {
+            return StudentCreateResponse.convertToStudent(existingStudent);
+        } else {
+            throw new Exception("BAD REQUEST");
+        }
+    }
+
+    public StudentCreateResponse updateStudent(Integer id,Student student) throws Exception {
+        Student existingStudent = studentRepository.findStudentById(id);
+        if (HelperUtils.isNotNull(existingStudent)) {
+            student.setCreatedDate(existingStudent.getCreatedDate());
+            student.setIsActive(existingStudent.getIsActive());
+            return StudentCreateResponse.convertToStudent(studentRepository.save(student));
+        } else {
+            throw new Exception(Constants.BAD_REQUEST);
+        }
+    }
+
+    public void deleteStudent(Integer id) throws Exception {
+        Student existingStudent = studentRepository.findStudentById(id);
+        if (HelperUtils.isNotNull(existingStudent)) {
+            existingStudent.setIsActive(Boolean.FALSE);
+            studentRepository.save(existingStudent);
+        } else {
+            throw new Exception(Constants.BAD_REQUEST);
+        }
+    }
+}
