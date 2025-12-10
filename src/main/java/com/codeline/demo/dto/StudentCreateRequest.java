2e8ff7dcb4f8ec9f487bdce2db7ecb9ad6218c80
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-10 10:07:42 +0400

    Created StudentCreateRequest

---
+++ src/main/java/com/codeline/demo/dto/StudentCreateRequest.java
@@ -0,0 +1,59 @@
+package com.codeline.demo.dto;
+
+import com.codeline.demo.entity.Address;
+import com.codeline.demo.entity.PhoneNumber;
+import com.codeline.demo.entity.Student;
+import com.codeline.demo.helper.Constants;
+import com.codeline.demo.helper.HelperUtils;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentCreateRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date dateOfBirth;
+    private String gender;
+    private List<String> phoneNumbers;
+    private Integer addressId;
+
+
+    public static Student convertToStudent(StudentCreateRequest request) {
+        return Student.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .dateOfBirth(request.getDateOfBirth())
+                .gender(request.getGender())
+                .build();
+    }
+
+    public static void validCreateStudentRequest(StudentCreateRequest request) throws Exception {
+        if (HelperUtils.isNull(request.getFirstName()) || request.getFirstName().isBlank()) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_FIRST_NAME_NOT_VALID);
+        } else if (HelperUtils.isNull(request.getLastName()) || request.getLastName().isBlank()) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_LAST_NAME_NOT_VALID);
+        } else if (HelperUtils.isNull(request.getEmail()) || request.getEmail().isBlank()) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_EMAIL_NOT_VALID);
+        } else if (HelperUtils.isNull(request.getDateOfBirth()) || request.getDateOfBirth().after(new Date())) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_DATE_OF_BIRTH_NOT_VALID);
+        } else if (HelperUtils.isNull(request.getGender()) || request.getGender().isBlank()) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_GENDER_NOT_VALID);
+        } else if (HelperUtils.isNull(request.getPhoneNumbers()) || request.getPhoneNumbers().isEmpty()) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_PHONE_NUMBER_NOT_VALID);
+        } else if (HelperUtils.isNull(request.getAddressId()) || request.getAddressId()<=0) {
+            throw new Exception(Constants.STUDENT_CREATE_REQUEST_ADDRESS_ID_NOT_VALID);
+        }
+    }
+}
