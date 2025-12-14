2024bdb0e0cdd3b6ad81ede965fc797fe0c04afc
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-12-14 17:04:35 +0400

    Updated PhoneNumberService

--- src/main/java/com/codeline/demo/service/PhoneNumberService.java
+++ src/main/java/com/codeline/demo/service/PhoneNumberService.java
@@ -1,20 +1,15 @@
 package com.codeline.demo.service;
 
-import com.codeline.demo.dto.PhoneNumberCreateRequest;
 import com.codeline.demo.dto.PhoneNumberCreateResponse;
-import com.codeline.demo.dto.StudentCreateResponse;
 import com.codeline.demo.entity.PhoneNumber;
-import com.codeline.demo.entity.Student;
 import com.codeline.demo.helper.Constants;
 import com.codeline.demo.helper.HelperUtils;
 import com.codeline.demo.repositories.PhoneNumberRepository;
-import com.codeline.demo.repositories.StudentRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -23,29 +18,6 @@ public class PhoneNumberService {
     @Autowired
     PhoneNumberRepository phoneNumberRepository;
 
-    @Autowired
-    StudentRepository studentRepository;
-
-
-    public PhoneNumberCreateResponse addPhoneNumber(Integer studentId, PhoneNumberCreateRequest request) throws Exception {
-        PhoneNumber phoneNumber = PhoneNumberCreateRequest.convertToPhoneNumber(request);
-        phoneNumber.setIsActive(Boolean.TRUE);
-
-        Student student = studentRepository.findStudentById(studentId);
-        if (HelperUtils.isNotNull(student)) {
-            student = studentRepository.save(student);
-        } else {
-            throw new Exception(Constants.PHONE_NUMBER_CREATE_REQUEST_STUDENT_ID_NOT_VALID);
-        }
-
-        List<PhoneNumber> phoneNumberList = new ArrayList<>();
-        phoneNumberList.add(phoneNumber);
-        student.setPhoneNumbers(phoneNumberList);
-        phoneNumber.setStudent(student);
-
-        return PhoneNumberCreateResponse.convertToPhoneNumber(phoneNumberRepository.save(phoneNumber));
-
-    }
 
     public PhoneNumberCreateResponse updatePhoneNumber(String phoneNumber, PhoneNumber request) throws Exception {
         PhoneNumber existingPhoneNumber = phoneNumberRepository.findPhoneNumberByNumber(phoneNumber);
@@ -65,5 +37,16 @@ public class PhoneNumberService {
         } else {
             throw new Exception(Constants.BAD_REQUEST);
         }
+    }
+
+    public List<PhoneNumberCreateResponse> getAllPhoneNumber(){
+        List<PhoneNumber> phoneNumbers = phoneNumberRepository.findByIsActiveTrue();
+        List<PhoneNumberCreateResponse> responsesList= new ArrayList<>();
+
+        for (PhoneNumber response : phoneNumbers){
+            PhoneNumberCreateResponse phoneResponse = PhoneNumberCreateResponse.convertToPhoneNumber(response);
+            responsesList.add(phoneResponse);
+        }
+        return responsesList;
     }
 }
